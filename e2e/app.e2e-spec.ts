4f718b339e470b0565e6f282804e0de265e5f19a
Resi Respati <resir014@gmail.com>
2017-09-15 01:43:37 +0700

    minor e2e fixes

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { AngularElectronPage } from './app.po';
 import { browser, element, by } from 'protractor';
 
-describe('angular-electron App', () => {
+describe('angular-electron-starter', () => {
   let page: AngularElectronPage;
 
   beforeEach(() => {
